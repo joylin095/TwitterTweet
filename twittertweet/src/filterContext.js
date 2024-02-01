3a427843578e6b9831ac7c8db7a4c82c1c5c997b
joylin <joylin000905@gmail.com>
2024-02-02 00:49:16 +0800

    新增預設初始資料

--- twittertweet/src/filterContext.js
+++ twittertweet/src/filterContext.js
@@ -1,12 +1,46 @@
 import React, { createContext, useContext, useState } from "react";
 
 const filterContext = createContext();
+let objectDate = new Date();
+let year = objectDate.getFullYear();
+let month = objectDate.getMonth() + 1;
+let day = objectDate.getDate();
+if (month < 10) {
+  month = "0" + month;
+}
+if (day < 10) {
+  day = "0" + day;
+}
+let date = `${year}-${month}-${day}`;
+const initUser = [
+  "sanyi_eth_",
+  "KuiGas",
+  "maik2hello",
+  "Greta0086",
+  "YourAirdropETH",
+  "ZF_lab",
+  "lubi366",
+  "DeFi8362",
+];
 
 export const FilterProvider = ({ children }) => {
   const [filters, setFilters] = useState(
     Array(10)
       .fill(null)
-      .map(() => ({ user: "", date: "", analytics: "" }))
+      .map((_, index) => {
+        if (initUser[index]) {
+          return {
+            user: initUser[index],
+            date: date,
+            analytics: 15000,
+          };
+        }
+        return {
+          user: "",
+          date: date,
+          analytics: 15000,
+        };
+      })
   );
   const [filterApplied, setFilterApplied] = useState(false);
   return (
