a4f4d4f52d1d2b981d0f0fddbefbf395aeac3338
joylin <joylin000905@gmail.com>
2024-02-02 23:35:23 +0800

    動態新增刪除左側filter

--- twittertweet/src/filter.js
+++ twittertweet/src/filter.js
@@ -3,6 +3,18 @@ import { Card, Button, Typography } from "@material-tailwind/react";
 import InputFilter from "./inputFilter";
 import { useFilter } from "./filterContext";
 
+let objectDate = new Date();
+let year = objectDate.getFullYear();
+let month = objectDate.getMonth() + 1;
+let day = objectDate.getDate();
+if (month < 10) {
+  month = "0" + month;
+}
+if (day < 10) {
+  day = "0" + day;
+}
+let date = `${year}-${month}-${day}`;
+
 export default function Filter() {
   const { filters, setFilters, filterApplied, setFilterApplied } = useFilter();
   const handleUserChange = (index, value) => {
@@ -26,6 +38,19 @@ export default function Filter() {
       return newFilters;
     });
   };
+  const removeInputFilter = (index) => {
+    setFilters((prevFilters) => prevFilters.filter((_, i) => i !== index));
+  };
+  const addInputFilter = () => {
+    setFilters((prevFilters) => [
+      ...prevFilters,
+      {
+        user: "",
+        date: date,
+        analytics: 15000,
+      },
+    ]);
+  };
   const repeatComponent = filters.map((filter, index) => (
     <InputFilter
       key={index}
@@ -35,6 +60,7 @@ export default function Filter() {
       onUserChange={(value) => handleUserChange(index, value)}
       onDateChange={(value) => handleDateChange(index, value)}
       onAnalyticsChange={(value) => handleAnalyticsChange(index, value)}
+      onRemove={() => removeInputFilter(index)}
     />
   ));
   const onSubmit = (e) => {
@@ -52,6 +78,9 @@ export default function Filter() {
       >
         <FilterButton filterApplied={filterApplied}></FilterButton>
         {repeatComponent}
+        <Button className="w-full" color="green" onClick={addInputFilter}>
+          Add Item
+        </Button>
       </form>
     </Card>
   );
--- twittertweet/src/inputFilter.js
+++ twittertweet/src/inputFilter.js
@@ -1,4 +1,4 @@
-import { Input, Typography } from "@material-tailwind/react";
+import { Input, Typography, IconButton } from "@material-tailwind/react";
 
 export default function InputFilter({
   user,
@@ -7,6 +7,7 @@ export default function InputFilter({
   onUserChange,
   onDateChange,
   onAnalyticsChange,
+  onRemove,
 }) {
   return (
     <div className="mb-5 flex flex-col gap-6 border-red-400 border-2">
@@ -49,6 +50,22 @@ export default function InputFilter({
         value={analytics}
         onChange={(e) => onAnalyticsChange(e.target.value)}
       />
+      <div className=" flex justify-center">
+        <IconButton color="red" variant="outlined" onClick={() => onRemove()}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </IconButton>
+      </div>
     </div>
   );
 }
